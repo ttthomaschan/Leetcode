0593fd30704b86e15bab81f1ad984ae42ecf7b7d
chenjl <chenjl@elimen.com.cn>
2021-07-26 21:30:06 +0800

    dp 打家劫舍系列

---
+++ 198.打家劫舍.py
@@ -0,0 +1,80 @@
+#
+# @lc app=leetcode.cn id=198 lang=python3
+#
+# [198] 打家劫舍
+#
+# https://leetcode-cn.com/problems/house-robber/description/
+#
+# algorithms
+# Medium (50.47%)
+# Likes:    1563
+# Dislikes: 0
+# Total Accepted:    334.8K
+# Total Submissions: 663.2K
+# Testcase Example:  '[1,2,3,1]'
+#
+# 
+# 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+# 
+# 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：[1,2,3,1]
+# 输出：4
+# 解释：偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+# 偷窃到的最高金额 = 1 + 3 = 4 。
+# 
+# 示例 2：
+# 
+# 
+# 输入：[2,7,9,3,1]
+# 输出：12
+# 解释：偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
+# 偷窃到的最高金额 = 2 + 9 + 1 = 12 。
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 1 
+# 0 
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def rob(self, nums):
+        if nums is None:
+            return 0
+        N = len(nums)
+        if N == 1: return nums[0]
+        if N == 2: return max(nums[0],nums[1])
+        '''常规-动态规划'''
+        # dp = [0]*N
+        # dp[0] = nums[0]
+        # dp[1] = max(nums[0],nums[1])
+        # for i in range(2,N):
+        #     dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+        # return max(dp)
+        '''状态压缩-动态规划'''
+        p = nums[0]
+        q = max(nums[0],nums[1])
+        for i in range(2,N):
+            cur = max(p+nums[i],q)
+            p = q
+            q = cur
+        return cur
+# @lc code=end
+
+if __name__ == "__main__":
+    nums = [0,0]
+    sol = Solution()
+    ans = sol.rob(nums)
+    print(ans)
